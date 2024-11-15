3389f82394a1551e997b2c7f69e591779cf6182f
avinautt12 <avinaluciano50@gmail.com>
2024-11-15 09:41:24 -0600

    login y registro

---
+++ app/src/main/java/com/example/integradoraiot/splash/SplashActivityLogin.java
@@ -0,0 +1,39 @@
+package com.example.integradoraiot.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.integradoraiot.MainActivity;
+import com.example.integradoraiot.R;
+
+public class SplashActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_login);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashActivityLogin.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        TextView registrarseText = findViewById(R.id.registro_text);
+        registrarseText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashActivityLogin.this, SplashActivityRegistro.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
