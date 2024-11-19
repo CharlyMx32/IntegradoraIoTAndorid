7123c5d4b5f1d12f2507d28c49208de0a2cb4574
CharlyMx32 <centenoavalosc@gmail.com>
2024-11-19 13:06:04 -0600

    redirecionamientos arreglados

--- app/src/main/java/com/example/integradoraiot/ui/SplashActivityFrag3.java
+++ app/src/main/java/com/example/integradoraiot/ui/SplashActivityFrag3.java
@@ -6,27 +6,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.example.integradoraiot.R;
 import com.example.integradoraiot.MainActivity;
+import com.example.integradoraiot.R;
 
 public class SplashActivityFrag3 extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Infla el layout del fragmento
         View view = inflater.inflate(R.layout.activity_frag3, container, false);
 
+        // Encuentra el botón usando 'view.findViewById'
         Button siguienteButton = view.findViewById(R.id.siguiente_Button);
 
-        siguienteButton.setOnClickListener(v -> {
-            Intent intent = new Intent(requireActivity(), MainActivity.class);
-            startActivity(intent);
-
-            requireActivity().finish();
+        // Configura el listener para el botón
+        siguienteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crea la intención para iniciar la siguiente actividad
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
         });
 
         return view;
