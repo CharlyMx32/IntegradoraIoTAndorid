b7e8e080a773ac9e139d2ac93c26f5eceaebad5e
CharlyMx32 <centenoavalosc@gmail.com>
2024-11-19 09:50:36 -0600

    las trancisiones corregidas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,26 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    // facebook
+    implementation ("com.facebook.android:facebook-applinks:latest.release")
+    implementation ("com.facebook.android:facebook-common:latest.release")
+    implementation ("com.facebook.android:facebook-core:latest.release")
+    implementation ("com.facebook.android:facebook-gamingservices:latest.release")
+    implementation ("com.facebook.android:facebook-login:latest.release")
+    implementation ("com.facebook.android:facebook-messenger:latest.release")
+    implementation ("com.facebook.android:facebook-share:latest.release")
+    implementation ("com.facebook.android:facebook-android-sdk:[4,5)")
+
+    // google Firebase
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+
+    // Also add the dependency for the Google Play services library and specify its version
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
+
 }
--- app/src/main/java/com/example/integradoraiot/models/RegisterRequest.java
+++ app/src/main/java/com/example/integradoraiot/models/RegisterRequest.java
@@ -2,13 +2,43 @@ package com.example.integradoraiot.models;
 
 public class RegisterRequest {
     private String email;
+    private String apellido;
+    private String sexo;
+    private String fechaNacimiento;
     private String password;
     private String nombre;
 
-    public RegisterRequest(String email, String password, String nombre) {
+    public RegisterRequest(String email, String password, String nombre, String apellido, String sexo, String fechaNacimiento) {
         this.email = email;
+        this.apellido = apellido;
+        this.sexo = sexo;
+        this.fechaNacimiento = fechaNacimiento;
         this.password = password;
         this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
     }
 
     // Getters y Setters (opcional si no usas Gson directamente)
