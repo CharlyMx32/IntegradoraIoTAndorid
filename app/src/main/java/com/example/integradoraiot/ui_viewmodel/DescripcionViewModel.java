f1b5ed80d41aa935045ff68c7c71926aec0aaa1e
avinautt12 <avinaluciano50@gmail.com>
2024-11-25 15:36:07 -0600

    DescripcionViewModel como game

--- app/src/main/java/com/example/integradoraiot/ui_viewmodel/DescripcionViewModel.java
+++ app/src/main/java/com/example/integradoraiot/ui_viewmodel/DescripcionViewModel.java
@@ -1,9 +1,12 @@
 package com.example.integradoraiot.ui_viewmodel;
 
+import android.content.Context;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.integradoraiot.TokenInterceptor;
+import com.example.integradoraiot.TokenManager;
 import com.example.integradoraiot.models.Descripcion;
 import com.example.integradoraiot.network.ApiService;
 import com.example.integradoraiot.network.RetroFitClient;
@@ -16,39 +19,35 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class DescripcionViewModel extends ViewModel {
-    // MutableLiveData que contiene la lista de descripciones
     private final MutableLiveData<List<Descripcion>> descripciones;
-    // Servicio de API para realizar la conexión a la red
     private final ApiService apiService;
 
-    // Constructor que inicializa las variables
-    public DescripcionViewModel() {
+    public DescripcionViewModel(Context context) {
         descripciones = new MutableLiveData<>();
-        apiService = RetroFitClient.getClient().create(ApiService.class);
+
+        TokenManager tokenManager = new TokenManager(context);
+        TokenInterceptor tokenInterceptor = new TokenInterceptor(tokenManager);
+
+        apiService = RetroFitClient.getClient(tokenInterceptor).create(ApiService.class);
     }
 
-    // Método para exponer el LiveData al observador
     public LiveData<List<Descripcion>> getDescripciones() {
         return descripciones;
     }
 
-    // Método para obtener las descripciones desde la API
     public void fetchDescripciones() {
         apiService.getDescripciones().enqueue(new Callback<List<Descripcion>>() {
             @Override
             public void onResponse(Call<List<Descripcion>> call, Response<List<Descripcion>> response) {
                 if (response.isSuccessful() && response.body() != null) {
-                    // Actualiza el MutableLiveData con la lista obtenida
                     descripciones.postValue(response.body());
                 } else {
-                    // Si la respuesta no es exitosa, envía una lista vacía
                     descripciones.postValue(Collections.emptyList());
                 }
             }
 
             @Override
             public void onFailure(Call<List<Descripcion>> call, Throwable t) {
-                // En caso de error en la llamada, envía una lista vacía
                 descripciones.postValue(Collections.emptyList());
             }
         });
