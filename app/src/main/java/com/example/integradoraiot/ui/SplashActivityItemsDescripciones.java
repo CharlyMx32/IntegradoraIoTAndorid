220eb0bb99c48a06ec201b02031e99c1677c5868
CharlyMx32 <centenoavalosc@gmail.com>
2024-11-28 08:58:46 -0600

    ya se puede seleccioanr el kid para jugar

--- app/src/main/java/com/example/integradoraiot/ui/SplashActivityItemsDescripciones.java
+++ app/src/main/java/com/example/integradoraiot/ui/SplashActivityItemsDescripciones.java
@@ -1,12 +1,18 @@
 package com.example.integradoraiot.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.example.integradoraiot.R;
+import com.example.integradoraiot.fragmentos.KidsFragment;
+import com.example.integradoraiot.fragmentos.SeleccionarKidsFragment;
 
 public class SplashActivityItemsDescripciones extends AppCompatActivity {
 
@@ -28,5 +34,19 @@ public class SplashActivityItemsDescripciones extends AppCompatActivity {
         gameName.setText(name);
         gameDescription.setText(description);
         gameImage.setImageResource(imageResource); // O usa Glide si es una URL
+
+        Button btnPlayGame = findViewById(R.id.btnPlayGame);
+        CardView cardFragmentContainer = findViewById(R.id.card_fragment_container);
+
+        btnPlayGame.setOnClickListener(v -> {
+            // Mostrar el contenedor como una tarjeta
+            cardFragmentContainer.setVisibility(View.VISIBLE);
+
+            // Opcional: Cargar un fragmento en el contenedor
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new SeleccionarKidsFragment())
+                    .commit();
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/integradoraiot/ui/SplashActivitySeleccionarKids.java
@@ -0,0 +1,8 @@
+package com.example.integradoraiot.ui;
+
+import com.example.integradoraiot.Adapters.GameAdapter;
+import com.example.integradoraiot.ui_viewmodel.GameViewModel;
+
+public class SplashActivitySeleccionarKids {
+
+}
