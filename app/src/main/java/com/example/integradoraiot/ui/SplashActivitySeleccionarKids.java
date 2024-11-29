0044f1a618178f013a8c578dee42dfbdc408cd0c
CharlyMx32 <centenoavalosc@gmail.com>
2024-11-29 12:55:12 -0600

    Revert "ya se puede seleccioanr el kid para jugar"
    This reverts commit 220eb0bb99c48a06ec201b02031e99c1677c5868.

--- app/src/main/java/com/example/integradoraiot/ui/SplashActivitySeleccionarKids.java
+++
@@ -1,8 +0,0 @@
-package com.example.integradoraiot.ui;
-
-import com.example.integradoraiot.Adapters.GameAdapter;
-import com.example.integradoraiot.ui_viewmodel.GameViewModel;
-
-public class SplashActivitySeleccionarKids {
-
-}
